24ad33920b9878df3697f48923e3990c33ade71c
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-14 00:23:21 +0200

    Initial commit

---
+++ src/Homework.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Homework {
+
+// Используйте класс Book (книга) из домашнего задания к уроку 45 (см. Book.java).
+// Создайте список книг и отсортируйте книги по названию, (при одинаковых названиях - по автору)
+// при помощи лямбда-функции.
+  public static void main(String[] args) {
+
+  }
+}
